67e34f594b6e1f7428f8c5b749e061387c828aa3
Adrian <63692318+adriand1az@users.noreply.github.com>
2021-01-20 00:07:07 -0600

    Hopefully this commit goes through

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,11 +11,41 @@ const firebaseConfig = {
     appId: "1:901196377729:web:4d5ebc1d936adb26502780",
     measurementId: "G-8XGKREVB87"
   };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get();
+
+    console.log(snapShot)
+
+    if (!snapShot){
+        const { displayName, email} = userAuth;
+        const createdAt = new Date()
+        
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+
+            })
+
+        } catch (error){
+            console.log('eror creating user', error.message)
+        }
+    }
+
+    return userRef;
+}
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
-
+export const firestore = firebase.firestore()
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
 
