9ac0a7dfabc39ce3297f4dd41764727131b03764
Adrian <63692318+adriand1az@users.noreply.github.com>
2021-01-20 22:31:05 -0600

    reformatted code added in reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
+import { createStructuredSelector } from "reselect";
 
 // Styles
 import './header.styles.scss';
@@ -11,6 +12,8 @@ import { ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 import CartIcon from'../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import {selectCartHidden} from '../../redux/cart/cart.selectors';
+import {selectCurrentUser} from '../../redux/user/user.selector'
 
 function Header ({currentUser, hidden}){
     return(
@@ -44,9 +47,9 @@ function Header ({currentUser, hidden}){
     )
 }
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}})  => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector ({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
